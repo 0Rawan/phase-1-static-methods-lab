97fb5fa21d4b9eff6931b0afb002765c62878d7f
Rawan <r.k096@hotmail.com>
2023-04-18 23:10:34 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    let pattern = /[^a-zA-Z0-9-' ]+/g;
+    return str.replace(pattern, '');
+  }
+
+  static titleize(str) {
+    let excludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let words = str.split(' ');
+
+    words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+
+    for (let i = 1; i < words.length; i++) {
+      if (!excludeWords.includes(words[i])) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+      }
+    }
+
+    return words.join(' ');
+  }
 }
